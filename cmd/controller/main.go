4b4d21c19674ca17007497beba31824aa2dfd0f8
Sellto <tom.selleslagh@gmail.com>
2021-05-10 21:13:25 +0200

    Update Name

--- cmd/controller/main.go
+++ cmd/controller/main.go
@@ -1,110 +1,92 @@
-//package main
-//
-//import (
-//	"context"
-//	"time"
-//
-//	//"flag"
-//	//"fmt"
-//	//log "github.com/sirupsen/logrus"
-//	//"io/ioutil"
-//	//"net/http"
-//	//"fmt"
-//	"github.com/cetic/kubeedge-controller/internal/config"
-//	"github.com/cetic/kubeedge-controller/internal/core"
-//	log "github.com/sirupsen/logrus"
-//	//"github.com/cetic/kubeedge-controller/internal/webserver"
-//	//"os"
-//	////"reflect"
-//	//"time"
-//)
-//
-////func Facetrack(w http.ResponseWriter, r *http.Request) {
-////  log.Println(d.GetStatus())
-////  if d.FSM.Current() == "run" {
-////    d.AddDesiredJob("Stop")
-////    d.PatchStatus()
-////    for d.FSM.Current() != "ready" {
-////      time.Sleep(500*time.Millisecond)
-////    }
-////  }
-////
-////  filename := "/app/dpu_face_tracking.py"
-////  url := ""
-////  go d.Launch(filename,url)
-////}
-////
-//func Passthrough() {
-//	if d.FSM.Current() == "run" {
-//		d.AddDesiredJob("Stop")
-//		d.PatchStatus()
-//		for d.FSM.Current() != "ready" {
-//			time.Sleep(500 * time.Millisecond)
-//		}
-//	}
-//
-//	filename := "/Users/tse/CETIC/viaduct/POCEdge/kubeedge-edge-worker/mapper/me-loop.py"
-//	url := ""
-//	go d.Launch(filename, url)
-//}
-//
-//func Passthrough2() {
-//	if d.FSM.Current() == "run" {
-//		d.AddDesiredJob("Stop")
-//		d.PatchStatus()
-//		for d.FSM.Current() != "ready" {
-//			time.Sleep(500 * time.Millisecond)
-//		}
-//	}
-//
-//	filename := "/Users/tse/CETIC/viaduct/POCEdge/kubeedge-edge-worker/mapper/hello-loop.py"
-//	url := ""
-//	go d.Launch(filename, url)
-//}
-//
-////
-////
-////func Stop(w http.ResponseWriter, r *http.Request) {
-////  if d.FSM.Current() == "run" {
-////    d.AddDesiredJob("Stop")
-////    d.PatchStatus()
-////    for d.FSM.Current() != "ready" {
-////      time.Sleep(500*time.Millisecond)
-////    }
-////  }
-////}
-//
-//
-//
-//
-//
-//
-//var d = core.Device{}
-//
-//func main() {
-//	log.SetLevel(log.DebugLevel)
-//	conf := config.Parse()
-//	log.Debugf("kubeconfig File: %s", conf.KubeConfig)
-//	log.Debugf("kubeconfig File: %s", conf.MasterAdress)
-//	crdClient, _ := core.NewCRDClient(conf.MasterAdress, conf.KubeConfig)
-//	d.InitDevice(conf.Device, "default", crdClient)
-//	ctx := context.Background()
-//	for {
-//		raw, _ := crdClient.Get().Namespace("default").Resource(core.ResourceTypeDevices).Name(conf.Device).DoRaw(ctx)
-//		log.Debugf("result: %s", raw)
-//		time.Sleep(1 * time.Second)
-//	}
-//
-//	//time.Sleep(10 * time.Second)
-//	//Passthrough()
-//	//time.Sleep(10 * time.Second)
-//	//Passthrough2()
-//	//time.Sleep(10 * time.Second)
-//	//s := new(web.Site)
-//	//s.Init()
-//	//s.AddPage("Home","gotpl/welcome.gohtml","/passthrough","home", Passthrough)
-//	//s.AddPage("Home","gotpl/welcome.gohtml","/facetrack","home", Facetrack)
-//	//s.AddPage("Home","gotpl/welcome.gohtml","/stop","home", Stop)
-//	//http.Handle("/", s.Mux)
-//	//http.ListenAndServe(":9090", nil)
-//}
+package main
+
+import (
+	"github.com/cetic/kubeedge-controller/internal/api"
+	"github.com/cetic/kubeedge-controller/internal/core"
+	"github.com/emicklei/go-restful"
+	restfulspec "github.com/emicklei/go-restful-openapi"
+	"github.com/go-openapi/spec"
+	log "github.com/sirupsen/logrus"
+	"net/http"
+)
+
+func sayHello(req *restful.Request, rsp *restful.Response) {
+	rsp.WriteEntity("Hello World")
+}
+
+func enrichSwaggerObject(swo *spec.Swagger) {
+	swo.Info = &spec.Info{
+		InfoProps: spec.InfoProps{
+			Title:       "ServiceName",
+			Description: "Description of the service",
+			Contact: &spec.ContactInfo{
+				Name:  "john",
+				Email: "john@doe.rp",
+				URL:   "http://johndoe.org",
+			},
+			License: &spec.License{
+				Name: "MIT",
+				URL:  "http://mit.org",
+			},
+			Version: "1.0.0",
+		},
+	}
+}
+
+func main() {
+	log.SetLevel(log.DebugLevel)
+	wc := restful.NewContainer()
+	const BasePath = "/api/v1"
+	u := core.Controller{}
+	u.Init()
+	ws := new(restful.WebService)
+	ws.Path(BasePath).
+		Consumes(restful.MIME_XML, restful.MIME_JSON).
+		Produces(restful.MIME_JSON, restful.MIME_XML) // you can specify this per route as well
+
+	tags := []string{"Hello"}
+	ws.Route(ws.GET("/hello").
+		To(sayHello).
+		Doc("Just Say Hello").
+		Metadata(restfulspec.KeyOpenAPITags, tags))
+
+	tags = []string{"Controller"}
+	ws.Route(ws.GET("/device/{deviceid}").
+		To(u.GetDevicebyId).
+		Doc("Get Device Status by ID").
+		Param(ws.PathParameter("deviceid", "Device ID").Required(true).DataType("string")).
+		Metadata(restfulspec.KeyOpenAPITags, tags))
+
+	ws.Route(ws.POST("/device/{deviceid}").
+		To(u.UpdateDevicebyId).
+		Doc("Get Device Status by ID").
+		Param(ws.PathParameter("deviceid", "Device ID").Required(true).DataType("string")).
+		Metadata(restfulspec.KeyOpenAPITags, tags).
+		Reads(api.Update{}).
+		Returns(200, "OK", ""))
+
+	ws.Route(ws.GET("/stop/{deviceid}").
+		To(u.StopDevicebyId).
+		Doc("Stop Device with ID").
+		Param(ws.PathParameter("deviceid", "Device ID").Required(true).DataType("string")).
+		Metadata(restfulspec.KeyOpenAPITags, tags))
+	wc.Add(ws)
+
+	// OpenAPI output documentation
+	config := restfulspec.Config{
+		WebServices:                   wc.RegisteredWebServices(),
+		APIPath:                       "/apidocs.json",
+		PostBuildSwaggerObjectHandler: enrichSwaggerObject}
+	restful.DefaultContainer.Add(restfulspec.NewOpenAPIService(config))
+	wc.Add(restfulspec.NewOpenAPIService(config))
+
+	// Optionally, you may need to enable CORS for the UI to work.
+	cors := restful.CrossOriginResourceSharing{
+		AllowedHeaders: []string{"Content-Type", "Accept"},
+		AllowedMethods: []string{"GET", "POST", "PUT", "DELETE"},
+		CookiesAllowed: false,
+		Container:      restful.DefaultContainer}
+	wc.Filter(cors.Filter)
+
+	log.Fatal(http.ListenAndServe(":8090", wc))
+}
